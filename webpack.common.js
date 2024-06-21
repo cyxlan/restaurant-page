6b8a9e24f14f70c7edf9d697b3e08e7c1c68939b
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-06-21 15:30:57 -0400

    Move js files into folder

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: './src/js/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -11,7 +11,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
