e7d43778b44e88b2d95233f5796c8d46d60af43c
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-06-21 16:02:08 -0400

    Fix adding undefined class if no classes specified

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,7 +1,9 @@
 function createEl(tagName, textContent, classes) {
   const el = document.createElement(tagName);
   el.textContent = textContent;
-  el.classList.add(classes);
+  if (classes) {
+    el.classList.add(classes);
+  }
   return el;
 }
 
