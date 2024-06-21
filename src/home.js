79f32c4da3ea44d55f78252229efdf60ab086ee7
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-06-21 15:22:39 -0400

    Move home tab content to module

---
+++ src/home.js
@@ -0,0 +1,7 @@
+export function homeTab(contentDiv) {
+  const header = document.createElement('h1');
+  header.textContent = "Restaurant";
+  const para = document.createElement('p');
+  para.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex rem dignissimos, non ad explicabo saepe animi dolor similique laudantium unde maiores quis nostrum expedita commodi dolore nihil fugit numquam aperiam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga eum, mollitia numquam animi dolore illum harum iusto quo sit dolor molestias nam nulla quia, quis ducimus ipsam deserunt pariatur corrupti.";
+  contentDiv.append(header, para);
+}
