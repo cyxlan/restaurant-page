476533b16dea2971ed7a31962aa599856bc71c9d
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-06-21 16:12:42 -0400

    Add switchTab function

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,22 @@ import "../css/index.css";
 
 const pageContent = (function() {
   const contentDiv = document.querySelector('#content');
-  homeTab(contentDiv);
+  const tabBtns = document.querySelectorAll('.tab-btn');
+
+  const switchTab = (btn) => {
+    contentDiv.textContent = "";
+    btn.classList.add("selected");
+    if (btn.id === "home") {
+      homeTab(contentDiv);
+    }
+  }
+
+  tabBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      switchTab(btn);
+    })
+  })
+
+  // default to home tab on page load
+  switchTab(tabBtns[0]);
 })();
