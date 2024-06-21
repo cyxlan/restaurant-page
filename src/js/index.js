da4413bb5a05e5d49b8e8b3e0e6f48883e16fe4b
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-06-21 15:37:32 -0400

    Set up css

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import { homeTab } from "./home";
+import "../css/reset.css";
+import "../css/index.css";
 
 const pageContent = (function() {
   const contentDiv = document.querySelector('#content');
