40760a2002e9824bd53c9a02acaafa6966b87178
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-08-24 18:37:18 -0400

    Style page bg & title

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,7 +2,7 @@ import { createEl } from "./tab";
 
 export function contactTab(contentDiv) {
   const els = [
-    createEl("h1", "Contact"),
+    createEl("h2", "Contact"),
     createEl("p", "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt veritatis, praesentium fuga dolore assumenda, similique incidunt totam id natus, nulla sed ipsam reiciendis deleniti. Earum excepturi quidem animi iure nam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum consectetur reprehenderit numquam perferendis placeat saepe adipisci molestiae voluptate, voluptatem, sequi officia sunt voluptatum odit facilis soluta tenetur cupiditate quos sit.")
   ];
   contentDiv.append(...els);
--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,7 @@ import { createEl } from "./tab";
 
 export function homeTab(contentDiv) {
   const els = [
-    createEl("h1", "Restaurant"),
+    createEl("h2", "Restaurant"),
     createEl("p", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex rem dignissimos, non ad explicabo saepe animi dolor similique laudantium unde maiores quis nostrum expedita commodi dolore nihil fugit numquam aperiam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga eum, mollitia numquam animi dolore illum harum iusto quo sit dolor molestias nam nulla quia, quis ducimus ipsam deserunt pariatur corrupti.")
   ];
   contentDiv.append(...els);
