0755f118205a0b8511bd56559b1f67768165c863
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-08-26 17:12:32 -0400

    Style menu page

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,5 +5,7 @@ export function menuTab(contentDiv) {
     createEl("h2", "Menu"),
     createEl("p", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium reiciendis, doloremque consequatur sed ex sint tenetur! Sint impedit error pariatur exercitationem, delectus inventore magnam doloremque, debitis aliquam sunt quam nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum fuga qui laudantium alias, hic accusantium facere veritatis quae ipsam natus reprehenderit similique ipsa nisi. Cupiditate, sint. Odio sunt omnis natus?")
   ];
-  contentDiv.append(...els);
+  const container = createEl("div", "", "container");
+  container.append(...els);
+  contentDiv.append(container);
 }
