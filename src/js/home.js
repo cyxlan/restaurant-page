9a3f6bd52a853c9db7443a8571196c8436fc9ae1
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-08-28 17:49:27 -0400

    Style home page

--- src/js/home.js
+++ src/js/home.js
@@ -1,9 +1,10 @@
 import { createEl } from "./tab";
 
 export function homeTab(contentDiv) {
-  const els = [
-    createEl("h2", "Restaurant"),
-    createEl("p", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex rem dignissimos, non ad explicabo saepe animi dolor similique laudantium unde maiores quis nostrum expedita commodi dolore nihil fugit numquam aperiam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga eum, mollitia numquam animi dolore illum harum iusto quo sit dolor molestias nam nulla quia, quis ducimus ipsam deserunt pariatur corrupti.")
-  ];
-  contentDiv.append(...els);
+  const container = createEl("div", "", ["container", "dark"]);
+  container.append(
+    createEl("p", "Shine True", ["motto"]),
+    createEl("p", "Fine wines and drinks served in the heart of Mondstadt.")
+  );
+  contentDiv.append(container);
 }
