61fe76233aec7a205a0054eb3ee86f592dbb6038
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-08-30 18:55:34 -0400

    Remove unused img file extensions

--- webpack.common.js
+++ webpack.common.js
@@ -22,7 +22,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        test: /\.(png|webp)$/i,
         type: "asset/resource",
       },
     ],
